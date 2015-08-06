8641399a03e6883d914d89187e268b249a639281
Bruno Casali <brunoocasali@gmail.com>
2015-08-06 01:26:44 -0300

    fixed locales, and responsivity of index pages

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,13 @@ Bundler.require(*Rails.groups)
 module MedicalCharts
   class Application < Rails::Application
     config.time_zone = 'Brasilia'
-    config.i18n.default_locale = :'pt-BR'
+    config.i18n.default_locale = 'pt-BR'
+
+    config.exceptions_app = self.routes
+
+    Dir.glob(Rails.root.join('config', 'locales', '**', '*/')).each do |dir|
+      paths['config/locales'] << dir
+    end
 
     config.active_record.raise_in_transactional_callbacks = true
 

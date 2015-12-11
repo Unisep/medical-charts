5a49e5af5ae17e038dbac409e67d949654f856ad
Bruno <brunoocasali@gmail.com>
2015-12-11 04:03:21 -0200

    just to commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,6 @@ Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
-
   config.before(:suite) { DatabaseCleaner.clean_with :truncation }
   config.before(:each) { DatabaseCleaner.strategy = :transaction }
   config.before(:each, js: true) { DatabaseCleaner.strategy = :truncation }
